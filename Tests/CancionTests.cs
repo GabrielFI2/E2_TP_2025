5039bba61affdc3aed378cd19dcf26ae22008c4c
Aarón Gabriel Ceballos Cano <aarongccfi2@gmail.com>
2025-05-20 02:20:31 -0600

    camb3

---
+++ Tests/CancionTests.cs
@@ -0,0 +1,28 @@
+using SistemaGestionMusica.Modelos;
+namespace SistemaGestionMusica.Tests
+{
+    public class CancionTests
+    {
+        [Fact]
+        public void Constructor_AsignarMetodoCorrectamente()
+        {
+            //Arrange
+            int duracion = 554;
+            var cancion = new Cancion("Bohemian Rhapsody", "Queen", 554);
+           
+
+            //Act
+
+            cancion.ToString();
+
+            //Asert 
+            Assert.Equal("Nombre esperado", cancion.Nombre);
+            Assert.Equal("Artista esperado", cancion.Artista);
+            Assert.Equal(duracion, cancion.DuracionSegundos);
+            Assert.Equal("Bohemian Rhapsody,- Queen (5:54)", cancion.ToString());
+        }
+        
+
+        
+    }
+}
